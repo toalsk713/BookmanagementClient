247aa95ce56f4da66b20368e42e28b6bc71516e6
choi sae mi na <alsk713@naver.com>
2016-11-07 15:48:49 +0900

    review 수정

--- js/review.js
+++ js/review.js
@@ -88,3 +88,48 @@
 
 
 
+
+	function searchBook(){
+		//입력상자에 키가 입력되면 무조건 호출
+		//우리가 원하는 건 enter key를 입력했을 때 서버와 통신
+		if(event.keyCode == 13){
+			//사용자가 입력한 ISBN번호를 가져와서
+			//AJAX로 서버프로그램을 호출
+
+			$.ajax({
+				url : "http://localhost:7070/book/bookList",
+				type : "GET",
+				dataType : "jsonp",
+				jsonp : "callback",
+				data : {
+					keyword : $("#place").val()
+				},
+				success : function(result){
+
+					for(var i = 0; i<result.length; i++) {
+
+						var tr = $("<tr></tr>").attr("data-isbn",result[i].isbn);
+
+						var titleTd = $("<td></td>").text(result[i].title);
+						var authorTd = $("<td></td>").text(result[i].author);
+
+						//서평
+						var commentbtn = $("<input />").attr("type","button").attr("value","서평").attr("id","comment").attr("class","btn-xs, btn-warning");
+						var commentTd = $("<td></td>").append(commentbtn);
+						
+						tr.append(titleTd);
+						tr.append(authorTd);
+						tr.append(commentTd);
+
+						$("tbody").append(tr);
+					}
+				},
+				error : function(){
+					alert("뭔가 이상함");
+				}
+
+			});
+		}
+	}
+
+
